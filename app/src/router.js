e007970fb3964c23623ed2216bf1fd68aa08b613
Nino Lambert <anthony.lamb001@gmail.com>
2022-10-21 17:28:44 -0400

    remove endpoints for starting and ending sessions
    Co-authored-by: Eugene O'Fallon <epofallon@gmail.com>
    Co-authored-by: Aaron Hull <aahull08@gmail.com>
    Co-authored-by: Marcin Kostecki <49038365+marcinkostecki@users.noreply.github.com>

--- app/src/router.js
+++ app/src/router.js
@@ -6,36 +6,6 @@ const router = express.Router();
 let dataService = new DataService();
 await dataService.init();
 
-//let us know that a session has begun
-router.post("/start-session", async (req, res) => {
-  let { sessionId, timestamp } = req.body;
-  if (!sessionId || !timestamp) {
-    res.status(400).send();
-  }
-  try {
-    await dataService.startNewSession(sessionId, timestamp);
-    res.status(200).send();
-  } catch (e) {
-    console.error(e);
-    res.status(500).send();
-  }
-});
-
-//let us know a session has ended
-router.post("/end-session", async (req, res) => {
-  let { sessionId, timestamp } = req.body;
-  if (!sessionId || !timestamp) {
-    res.status(400).send();
-  }
-  try {
-    await dataService.endSession(sessionId, timestamp);
-    res.status(200).send();
-  } catch (e) {
-    console.error(e);
-    res.status(500).send();
-  }
-});
-
 //take session data from the body and feed it to the data pipeline
 router.post("/record", async (req, res) => {
   let { sessionId, events } = req.body;
