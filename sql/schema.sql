bbb14f2e70fb5c3d6f7c92b8d34574ae91d3df87
Eugene O'Fallon <70714033+epofallon@users.noreply.github.com>
2022-10-27 11:38:54 -0500

    update postgres schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -9,3 +9,11 @@ CREATE TABLE IF NOT EXISTS pending_sessions (
   origin_host text NOT NULL,
   most_recent_event_time bigint NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS funnels (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  created_at_ms bigint NOT NULL,
+  last_modified_at_ms bigint NOT NULL,
+  funnel JSONB NOT NULL
+);
